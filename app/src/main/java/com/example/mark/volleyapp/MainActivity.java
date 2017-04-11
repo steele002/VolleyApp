3dc3e58348c84449da2c3e39fa1b65d6a8a8c584
steele002 <steelram62@gmail.com>
2017-04-10 20:54:07 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mark/volleyapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mark.volleyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "http://httpbin.org/html";
+        final TextView text = (TextView)findViewById(R.id.text);
+// Request a string response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        // Result handling
+                        Log.d("DEBUG", "testing");
+                        text.setText(response);
+                        System.out.println(response.substring(0,200));
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                System.out.println("Something went wrong!");
+                error.printStackTrace();
+
+            }
+        });
+
+// Add the request to the queue
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+}
